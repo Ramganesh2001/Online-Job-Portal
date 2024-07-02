02aef22e2cc0e940a19d80885c22ed3842bdfcc3
RAMGANESH P <123757277+Ramganesh2001@users.noreply.github.com>
2024-07-02 17:15:45 +0530

    Add files via upload

---
+++ admin/About.php
@@ -0,0 +1,85 @@
+ 
+
+<section id="content">
+	<section class="section-padding">
+		<div class="container">
+			<div class="row showcase-section">
+				<div class="col-md-12">
+					<div class="about-logo">
+						<h3>Why Choose Us?</h3>
+					
+						<p>JobsCenter is your trusted recruitment partner in the search of new employees. Our recruitment services are tailored to match your exact staffing needs in the short and the long term, and guarantee the best candidate is placed in the right place. Working with us will help you make a more assured hiring decision and will save your business great amounts of time, money and effort.</p>
+
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</section>
+	<div class="container">
+					
+					<div class="about">
+						<br>
+						<!-- Our Team starts -->
+				
+						<!-- Heading -->
+						<div class="block-heading-six">
+							<h4 class="bg-color">Our Team</h4>
+						</div>
+						<br>
+						
+						<!-- Our team starts -->
+						
+						<div class="team-six">
+							<div class="row">
+								<div class="col-md-3 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="images/1.jpeg" alt="">
+										<!-- Name -->
+										<h4>Ramganesh</h4>
+										<span class="deg">Creative</span> 
+									</div>
+								</div>
+								<div class="col-md-3 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="img/team2.jpg" alt="">
+										<!-- Name -->
+										<h4>Nazir</h4>
+										<span class="deg">Programmer</span> 
+									</div>
+								</div>
+								<div class="col-md-3 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="img/team3.jpg" alt="">
+										<!-- Name -->
+										<h4>Ramganesh</h4>
+										<span class="deg">CEO</span> 
+									</div>
+								</div>
+								<div class="col-md-3 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="img/team4.jpg" alt="">
+										<!-- Name -->
+										<h4>Rahul</h4>
+										<span class="deg">Manager</span> 
+									</div>
+								</div>
+							</div>
+						</div>
+						
+						<!-- Our team ends -->
+					  
+						
+					</div>
+									
+				</div>
+	</section> 
+ 
